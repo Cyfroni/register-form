e66a108b7f093e16ca02e452cd7e8859502815e3
Karol <karolplg@gmail.com>
2019-11-17 02:57:05 +0100

    simple register form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,27 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" style>
+    <RegisterForm />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import RegisterForm from './components/RegisterForm.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    RegisterForm
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background-image: url('https://wedio-cdn.s3.eu-west-1.amazonaws.com/images/homepage/login/login_bg_x3.jpg');
+  background-position: right;
+  background-size: cover;
+
+  min-height: 100vh;
+  position: relative;
 }
 </style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <form>
+    <h1>WELCOME TO WEDIO -</h1>
+    <p>the fastest-growing European community of creators</p>
+    <div class="form-group">
+      <label for="EmailInput">Email</label>
+      <input
+        type="email"
+        class="form-control"
+        id="EmailInput"
+        placeholder="Enter email"
+      />
+    </div>
+    <div class="form-group">
+      <label for="UsernameInput">Username</label>
+      <input
+        type="email"
+        class="form-control"
+        id="UsernameInput"
+        placeholder="Enter username"
+      />
+    </div>
+    <div class="form-group">
+      <label for="InputPassword">Password</label>
+      <input
+        type="password"
+        class="form-control"
+        id="InputPassword"
+        placeholder="Enter password"
+      />
+    </div>
+    <small id="emailHelp" class="form-text text-muted"
+      >By clicking on Register, you agree to Wedio's Terms & Conditions and
+      Privacy Policy..</small
+    >
+    <button type="button" class="btn btn-primary">
+      REGISTER
+    </button>
+    <button type="button" class="btn btn-outline-primary">
+      SIGN IN
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'RegisterForm'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.btn {
+  margin: 50px;
+  border-radius: 30px !important;
+}
+
+form {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+</style>
